9953f29cf5b4cbe326451b27f68cab392887b3b8
ningmeng <hyoobo@gmail.com>
2023-06-22 18:15:57 +0800

    fix:提高代码可读性

--- archive_wechat_file.py
+++ archive_wechat_file.py
@@ -5,6 +5,12 @@ import hashlib
 import datetime
 import filecmp
 
+root_dir=(
+    ''  # 微信文件所在的路径，不用加/
+)
+
+transit_path = '' # 中转站的路径，最后要加上/
+target_path2 = '' # 用于存储复制过来的文件的目标目录，最后要加上/
 
 def TimeStampToTime(timestamp):
     timeStruct = time.localtime(timestamp)
@@ -12,9 +18,8 @@ def TimeStampToTime(timestamp):
 
 
 def get_change_time(file_path):
-    chuangjian = TimeStampToTime(os.stat(file_path).st_ctime)
-    # print(chuangjian)  # 文件的添加时间
-    return chuangjian
+    creation_time = TimeStampToTime(os.stat(file_path).st_ctime)
+    return creation_time
 
 
 def return_md5(file):
@@ -25,8 +30,7 @@ def return_md5(file):
 
 
 def mkdir_target(riqi):
-    # 目标文件
-    path = ''
+    path = target_path2  # 目标文件
     try:
         os.mkdir(path + str(riqi))
     except:
@@ -34,71 +38,65 @@ def mkdir_target(riqi):
 
 
 def return_target_file_name(target_path):
-    # 返回的不带路径
-    a = []
-    g = os.walk(target_path)
-    for path, dir_list, file_list in g:
-        for file_list in file_list:
-            a.append(file_list)
-    return a
+    # Returns filenames without the path
+    filenames = []
+    for path, dir_list, file_list in os.walk(target_path):
+        for file_name in file_list:
+            filenames.append(file_name)
+    return filenames
 
 
 def scanning_file(source_path2):
-    # source 是 mac 微信的路径
-    # targee 是 目标 的路径
-    # 要检查的文件夹
-    g = os.walk(source_path2)
-    for path, dir_list, source_file_list in g:
-        # 第一个循环，遍历所有source文件
-        for source_file_list in source_file_list:
-            # 第二个循环，遍历所有souce文件名字
-            riqi = get_change_time(str(path) + '/' + str(source_file_list))
-            target_path = '' + riqi
-            target_path_file_name = return_target_file_name(target_path)
-            # 获取目标文件夹内的文件名
+    """
+    source 是 mac 微信的路径
+    targee 是 目标 的路径
+    要检查的文件夹
+    """
+    file_tree = os.walk(source_path2)
+    for path, dir_list, source_file_list in file_tree:  # 第一个循环，遍历所有source文件
+        for source_file_list in source_file_list:  # 第二个循环，遍历所有souce文件名字
 
             # 防止找不到riqi文件而直接跳过for导致报错
-            status = 3
+            riqi = get_change_time(str(path) + '/' + str(source_file_list))
+            target_path = target_path2 + riqi
+            target_path_file_name = return_target_file_name(target_path)  # 获取目标文件夹内的文件名
 
+            status = 3
             for i in target_path_file_name:
+                # 文件名相同
                 if i == source_file_list:
                     md5_target = return_md5(target_path + '/' + i)
                     md5_source = return_md5(path + '/' + source_file_list)
 
+                    # MD5相同（两个重名的文件
                     if md5_target == md5_source:
                         status = 2
                         break
+
+                    # MD5不同（两个重名的文件
                     else:
-                        # 重新查md5
-                        print(i)
-                        target_path_file_name.remove(i)
-                        for uu in target_path_file_name:
-                            md5_target = return_md5(target_path + '/' + uu)
-                            md5_source = return_md5(path + '/' + source_file_list)
-                            if uu == target_path_file_name:
-                                status = 1
-                                break
-                            else:
-                                status = 2
+                        status = 1
                         break
 
                 else:
                     status = 3
-            # status ==
-            # 1: 名字一样，md5不一样，重命名复制
-            # 2：名字一样，md5一样，跳过  or 名字不一样，md5一样，跳过
-            # 3：名字不一样，直接复制
+            print(status)
 
+            """
+            status
+            1: 名字一样，md5不一样，重命名复制
+            2：名字一样，md5一样，跳过
+            3：名字不一样，直接复制
+            """
             if status == 3:
-                # 名字不一样
+
                 mkdir_target(riqi)
-                target_path = '' + riqi
-                # print(path)
+                target_path = target_path2 + riqi
                 try:
                     shutil.copy2(str(path) + '/' + str(source_file_list), target_path)
                 except:
                     print(source_file_list)
-                print('复制了：', source_file_list)
+                print('复制了：', source_file_list,target_path)
 
             elif status == 1:
                 # 如果md5不一样而且名字一样
@@ -107,10 +105,7 @@ def scanning_file(source_path2):
                 shijian = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
 
                 # 迁移到中转站
-                # z中转站路径
-                transit_path = ''
-
-
+                # 中转站路径
                 shutil.copy2(str(path) + '/' + str(source_file_list), transit_path + source_file_list)
 
                 # 在中转处重命名
@@ -122,7 +117,7 @@ def scanning_file(source_path2):
                 # 复制重命名后到目标目录
                 mkdir_target(riqi)
                 try:
-                    shutil.copy2(str(transit_path) + '/' + str(file_name), target_path)
+                    shutil.move(str(transit_path) + '/' + str(file_name), target_path)
                 except:
                     print(file_name)
                 print('重命名了：', file_name, md5_target, md5_source)
@@ -131,19 +126,19 @@ def scanning_file(source_path2):
                 pass
 
 
-def get_wx_file_path():
-    # 要检查的文件夹，注意替换
-    g = os.walk(
-        '/Users/#你的用户名#/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/**/**/Message/MessageTemp')
-    a = []
-    for path, dir_list, file_list in g:
-        if path.find('File') != -1 or path.find('OpenData') != -1:
-            a.append(path)
-    return a
+def get_wechat_file_paths():
+    # 定义要扫描的目录，替换成你实际的目录
+
+    wechat_paths = []
+    for current_path, directories, files in os.walk(root_dir):
+        if 'File' in current_path or 'OpenData' in current_path:
+            wechat_paths.append(current_path)
+    return wechat_paths
 
 
 while 1:
-    work_path = get_wx_file_path()
+    work_path = get_wechat_file_paths()
+    print(work_path)
     for i in range(len(work_path)):
         scanning_file(work_path[i])
-    time.sleep(200)
+    break
